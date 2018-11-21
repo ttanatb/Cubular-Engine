e8064fc134a58f7422d82d28811ac8eff43d1373
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2018-11-20 21:11:02 -0500

    Added a server address argument

--- CubularEngine/Main.cpp
+++ CubularEngine/Main.cpp
@@ -5,11 +5,13 @@
 
 #include "TankServer.h"
 #include "TankClient.h"
+                         
+#define IS_CLIENT_FLAG   "-c"
+#define IS_SERVER_FLAG   "-s"
+#define PORT_NUM_FLAG    "-p"
+#define HELP_FLAG        "-h"
+#define SERVER_ADDR_FLAG "-a"
 
-#define IS_CLIENT_FLAG "-c"
-#define IS_SERVER_FLAG "-s"
-#define PORT_NUM_FLAG  "-p"
-#define HELP_FLAG      "-h"
 
 
 /// <summary>
@@ -17,10 +19,11 @@
 /// </summary>
 void PrintHelp()
 {
-    fprintf( stderr, "\nUSAGE: ./UDP_Tank.exe [-c] [-s] [-p PORT_NUM] \n" );
+    fprintf( stderr, "\nUSAGE: ./UDP_Tank.exe [-c] [-a] [-s] [-p PORT_NUM] \n" );
     fprintf( stderr, "\nOptions: \n" );
     fprintf( stderr, "\t-h\tPrint this helpful message.\n" );
     fprintf( stderr, "\t-c\tRun this application as a client. (Default: true)\n" );
+    fprintf( stderr, "\t-a\tThe server address to connect to. (Default: 127.0.0.1)\n" );
     fprintf( stderr, "\t-s\tRun this application as a server. (Default: true)\n" );
     fprintf( stderr, "\t-p\tPort number to run on.            (Default: 50000)\n" );
 }
@@ -46,6 +49,7 @@ int main( int argc, char* argv[] )
     bool isClient = false;
 
     int PORT = 50000;
+    char SERVER_ADDR[16] = "127.0.0.1\0";
 
     // Loop through the args and configure as necessary
     for ( int i = 0; i < argc; ++i )
@@ -72,6 +76,13 @@ int main( int argc, char* argv[] )
                 sscanf_s( argv[ i + 1 ], "%d", &PORT );
             }
         }
+        else if ( strcmp( argv[ i ], SERVER_ADDR_FLAG ) == 0 )
+        {
+            if ( i + 1 < argc )
+            {
+                strcpy_s( SERVER_ADDR, argv[ i + 1 ] );
+            }
+        }
     }
 
     // Server Scope ---------------
@@ -87,7 +98,7 @@ int main( int argc, char* argv[] )
     {
         if ( isClient )
         {
-            Client = std::make_unique<Networking::TankClient>( "127.0.0.1", PORT );
+            Client = std::make_unique<Networking::TankClient>( SERVER_ADDR, PORT );
 
             Engine engine = Engine(Client.get());
             if ( engine.Init() == 0 )
